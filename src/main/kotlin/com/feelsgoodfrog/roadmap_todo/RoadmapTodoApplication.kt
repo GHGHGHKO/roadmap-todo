c5298565a80248294473f3eea7feb8df6a34c3b7
GHGHGHKO <gudrb963@gmail.com>
2024-11-10 19:46:54 +0900

    feat: Initial commit

---
+++ src/main/kotlin/com/feelsgoodfrog/roadmap_todo/RoadmapTodoApplication.kt
@@ -0,0 +1,11 @@
+package com.feelsgoodfrog.roadmap_todo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoadmapTodoApplication
+
+fun main(args: Array<String>) {
+	runApplication<RoadmapTodoApplication>(*args)
+}
---
+++ src/test/kotlin/com/feelsgoodfrog/roadmap_todo/RoadmapTodoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.feelsgoodfrog.roadmap_todo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RoadmapTodoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

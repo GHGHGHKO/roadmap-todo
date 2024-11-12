fcdd7b9fb196c38d3626e8abfcfef1bcf33ebb48
GHGHGHKO <gudrb963@gmail.com>
2024-11-12 23:23:33 +0900

    feat(Users): delete `@GeneratedValue`

--- src/main/kotlin/com/feelsgoodfrog/roadmap_todo/domain/user/entity/Users.kt
+++ src/main/kotlin/com/feelsgoodfrog/roadmap_todo/domain/user/entity/Users.kt
@@ -2,15 +2,13 @@ package com.feelsgoodfrog.roadmap_todo.domain.user.entity
 
 import com.feelsgoodfrog.roadmap_todo.common.BaseEntity
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import java.util.*
 
 @Entity
 class Users(
-    @Id @GeneratedValue(strategy = GenerationType.UUID)
-    val id: UUID = UUID.randomUUID(),
+    @Id
+    val id: String = UUID.randomUUID().toString(),
 
     val userName: String,
 

bc3ff03ed97bfec1c86567da0fd6061841f7940b
GHGHGHKO <gudrb963@gmail.com>
2024-12-01 15:43:42 +0900

    feat(advice): add GlobalExceptionAdvice

---
+++ src/main/kotlin/com/feelsgoodfrog/roadmap_todo/common/advice/GlobalExceptionAdvice.kt
@@ -0,0 +1,53 @@
+package com.feelsgoodfrog.roadmap_todo.common.advice
+
+import com.feelsgoodfrog.roadmap_todo.common.dto.AdviceResponseDto
+import com.feelsgoodfrog.roadmap_todo.common.exception.UserExistsException
+import com.feelsgoodfrog.roadmap_todo.common.exception.UserNotFoundException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.lang.IllegalArgumentException
+
+@RestControllerAdvice
+class GlobalExceptionAdvice {
+
+    @ExceptionHandler(
+        value = [
+            UserExistsException::class,
+            IllegalArgumentException::class,
+            UserNotFoundException::class,
+            UsernameNotFoundException::class
+        ]
+    )
+    @ResponseBody
+    fun badRequestException(
+        e: Exception
+    ): ResponseEntity<AdviceResponseDto> {
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(e.message?.let {
+                AdviceResponseDto(
+                    message = it
+                )
+            })
+    }
+
+    @ExceptionHandler(value = [IllegalAccessException::class])
+    @ResponseBody
+    fun forbiddenException(
+        e: IllegalAccessException
+    ): ResponseEntity<AdviceResponseDto> {
+        return ResponseEntity
+            .status(HttpStatus.FORBIDDEN)
+            .body(
+                e.message?.let {
+                    AdviceResponseDto(
+                        message = it
+                    )
+                }
+            )
+    }
+}
---
+++ src/main/kotlin/com/feelsgoodfrog/roadmap_todo/common/dto/AdviceResponseDto.kt
@@ -0,0 +1,5 @@
+package com.feelsgoodfrog.roadmap_todo.common.dto
+
+data class AdviceResponseDto(
+    val message: String
+)
